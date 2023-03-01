775862afbca10d2e5f3ce1a48eba49b466879bc8
cloudedpanther <rickeykoh@ajou.ac.kr>
2023-03-01 20:49:29 +0900

    complete

--- app.js
+++ app.js
@@ -1,1 +1,67 @@
-// do something!
+(function () {
+  "use-strict";
+
+  const get = (dom) => document.querySelector(dom);
+
+  const NAVIGAGTION_ACTIVE_KEY = "navigation-active-key";
+
+  class Navigation {
+    constructor(target, trigger) {
+      this.target = target;
+      this.trigger = trigger;
+      this.active = this.setActivity();
+    }
+
+    setActivity() {
+      const isActive = localStorage.getItem(
+        NAVIGAGTION_ACTIVE_KEY
+      );
+
+      return isActive === "true";
+    }
+
+    toggleNavigation = () => {
+      this.target.classList.toggle("active");
+      this.active = !this.active;
+      console.log(this.active);
+      localStorage.setItem(
+        NAVIGAGTION_ACTIVE_KEY,
+        this.active
+      );
+    };
+
+    init = () => {
+      this.trigger.addEventListener(
+        "click",
+        this.toggleNavigation
+      );
+
+      if (this.active) {
+        this.target.classList.add("active");
+      }
+    };
+  }
+
+  const finishPreload = () => {
+    const body = get("body");
+
+    body.classList.remove("preload");
+    body.style.visibility = "visible";
+  };
+
+  const init = () => {
+    const toggleNavButtonDOM = get(".toggle");
+    const navigationDOM = get("nav");
+
+    const navigation = new Navigation(
+      navigationDOM,
+      toggleNavButtonDOM
+    );
+
+    navigation.init();
+
+    window.addEventListener("load", finishPreload);
+  };
+
+  init();
+})();
